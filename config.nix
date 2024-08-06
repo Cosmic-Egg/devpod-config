27de0c86b73cde4c748ea8efcde47e2138a7b75b
Cosmic-Egg <ekemeny18@gmail.com>
2024-08-06 17:07:26 -0600

    added initial files

---
+++ config.nix
@@ -0,0 +1,19 @@
+{
+  packageOverrides = pkgs: with pkgs; {
+    myPackages = pkgs.buildEnv {
+      name = "esk-tools";
+      paths = [
+        bash-completion
+        neovim
+        nodejs_22
+        starship
+        fd
+        ripgrep
+        fzf
+        lazygit
+        fastfetch
+        yazi
+      ];
+    };
+  };
+}
